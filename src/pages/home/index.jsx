f99d9b0d91ca761221281fbed95ad5dd1ff392ea
吳俊緯 <wcw850710@gmail.com>
2021-02-25 10:33:05 +0800

    feat(app): add basic router

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './style.module.scss'
+
+function Home() {
+	return (
+		<div className={styles.home}>
+			<h1>welcome to link-me home page!</h1>
+		</div>
+	)
+}
+
+export default Home
