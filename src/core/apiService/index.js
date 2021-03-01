b2c3d2cc7d89fb99c02d7e4cf8ae547c34c7ed73
吳俊緯 <wcw850710@gmail.com>
2021-03-01 22:38:40 +0800

    initial interceptors and example

--- src/core/apiService/index.js
+++ src/core/apiService/index.js
@@ -1,9 +1,32 @@
 import axios from 'axios'
 
-// axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL
+
+axios.interceptors.request.use(
+	config => {
+		return config
+	},
+	error => {
+		return Promise.resolve(error)
+	},
+)
+
+axios.interceptors.response.use(
+	response => {
+		return response
+	},
+	error => {
+		const res = error.response
+		return Promise.resolve(res)
+	},
+)
+
+const apis = {
+	login: params => axios.post('login', params)
+}
 
 function installApi() {
-	window.$api = axios
+	window.$apis = apis
 }
 
 export default installApi
