2ace5551c352559cb171659b4a184117d5c1fc6b
吳俊緯 <wcw850710@gmail.com>
2021-03-05 13:34:55 +0800

    lib: 新增 Arr

--- src/lib/Arr.js
+++ src/lib/Arr.js
@@ -1,1 +1,32 @@
-export default {}
+function update(index, value) {
+	return arr => [...arr.slice(0, index), value, ...arr.slice(index + 1)]
+}
+
+function push(...value) {
+	return arr => [...arr, ...value]
+}
+
+function unshift(...value) {
+	return arr => [...value, ...arr]
+}
+
+function pop() {
+	return arr => arr.slice(0, -1)
+}
+
+function shift() {
+	return arr => arr.slice(1)
+}
+
+function splice(from, to, ...value) {
+	return arr => [...arr.slice(0, from), ...value, ...arr.slice(from + to)]
+}
+
+export default {
+	update,
+	unshift,
+	push,
+	pop,
+	shift,
+	splice,
+}
