8d4124f58f536cebc8517a5dcab7f7a314c3aef1
吳俊緯 <wcw850710@gmail.com>
2021-03-05 08:52:07 +0800

    hooks: 將 hooks 統一導出

---
+++ src/hooks/index.js
@@ -0,0 +1,4 @@
+import useLocalStorageState from '@/hooks/storage/useLocalStorageState'
+import useSessionStorageState from '@/hooks/storage/useSessionStorageState'
+
+export { useLocalStorageState, useSessionStorageState }
