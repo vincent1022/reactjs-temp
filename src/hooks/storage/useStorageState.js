acc718f147135bbb79de86e5c9ee2d110cfbb636
吳俊緯 <wcw850710@gmail.com>
2021-03-05 13:54:00 +0800

    lib: useStorageState 改為閉包

--- src/hooks/storage/useStorageState.js
+++ src/hooks/storage/useStorageState.js
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react'
 
-const CACHE = {}
-function useStorageState(symbol, initialValue) {
-	const [state, setState] = useState(CACHE[symbol] ?? initialValue)
-	useEffect(() => {
-		CACHE[symbol] = state
-	}, [state])
-	return [state, setState]
-}
+const useStorageState = (() => {
+	const CACHE = {}
+	return (symbol, initialValue) => {
+		const [state, setState] = useState(CACHE[symbol] ?? initialValue)
+		useEffect(() => {
+			CACHE[symbol] = state
+		}, [state])
+		return [state, setState]
+	}
+})()
 
 export default useStorageState
