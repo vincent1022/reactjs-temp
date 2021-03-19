2932ef282bff9287732a98b4802cbbc1b2004021
吳俊緯 <wcw850710@gmail.com>
2021-03-19 08:48:01 +0800

    package: 路由升級

--- src/core/app/routes.jsx
+++ src/core/app/routes.jsx
@@ -1,5 +1,5 @@
 import { lazy } from 'react'
-import { RouterGenerator } from 'jsl/react/lib'
+import { createRoutes } from 'jsl/react/lib'
 const Home = lazy(() => import('@/pages/Home'))
 const Example = lazy(() => import('@/pages/Example'))
 
@@ -23,6 +23,4 @@ const routes = [
 	},
 ]
 
-const Routes = RouterGenerator.init(routes)
-
-export { Routes }
+export const { Routes } = createRoutes(routes)
